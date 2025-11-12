c702c3598c099717159189999501140ac7c5b9a2
Raul <rgarreta@gmail.com>
2025-11-12 09:28:02 -0800

    section 6

---
+++ autonomy_repo/scripts/perception_controller.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import rclpy
+
+from asl_tb3_lib.control import BaseHeadingController
+from asl_tb3_lib.math_utils import wrap_angle
+from asl_tb3_msgs.msg import TurtleBotControl, TurtleBotState
+
+
+class PerceptionController(BaseHeadingController):
+
+    def __init__(self, node_name: str) -> None:
+        super().__init__(node_name)
+        
+        self.declare_parameter("kp",2.0)
+
+        self.declare_parameter("active",2.0)
+    
+    @property
+    def set_kp(self) -> float:
+        return self.get_parameter("kp").value
+
+    @property
+    def active(self) -> bool:
+        return self.get_parameter("active").value
+    
+    def compute_control_with_goal(self, state: TurtleBotState, goal: TurtleBotState) -> TurtleBotControl:
+        error = wrap_angle(goal.theta - state.theta)
+        # omega = self.get_parameter("kp").value * error
+        if self.active:
+            omega = 0.2
+        else:
+            omega = 0.0
+        return TurtleBotControl(v=0.0, omega=omega)
+        
+
+
+if __name__ == "__main__":
+    rclpy.init()
+    node = PerceptionController("perception_controller")
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+    
